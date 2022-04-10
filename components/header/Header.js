24890eb7e0dca2ae31c4c5bcd7e09bfbc089043a
Shubham Prasad <shubhamprasad63@gmail.com>
2022-04-10 10:34:37 +0530

    Add non-interactive nav bar

--- components/header/Header.js
+++ components/header/Header.js
@@ -1,4 +1,22 @@
+import NavBar from "./NavBar";
+
+const navBarItems = [
+  { id: 1, text: "Laptops" },
+  { id: 2, text: "TVs" },
+  { id: 3, text: "Audio" },
+  { id: 4, text: "Gaming" },
+  { id: 5, text: "Smart Home" },
+  { id: 6, text: "Cart" },
+  { id: 7, text: "Profile" },
+];
+
 const Header = () => {
+  return (
+    <header className="flex justify-between align-between mt-2 mx-4 pb-2 border-b-2">
+      <div className="text-4xl font-bold text-blue-500">Creators Hub</div>
+      <NavBar items={navBarItems} />
+    </header>
+  );
 };
 
 export default Header;
