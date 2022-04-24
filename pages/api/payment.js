d0ec882076e85e1a78964b6c439407b255a55839
Shivam <chotu.kv@gmail.com>
2022-04-24 14:54:04 +0530

    One File API

--- pages/api/payment.js
+++ pages/api/payment.js
@@ -2,13 +2,75 @@ import { NodeNextRequest } from 'next/dist/server/base-http/node';
 
 const sdk = require('api')('@circle-api/v1#9kvo29l1wa6b24');
 
+ async function encryptDetails(){
+       const decodedPublicKey = await readKey({
+        armoredKey: Buffer.from(publicKey, 'base64').toString(),
+    });
 
+    const message = await createMessage({
+        text: JSON.stringify(dataToEncrypt),
+    });
+
+    return encrypt({
+        message,
+        encryptionKeys: decodedPublicKey,
+    }).then((ciphertext) => {
+        return {
+            encryptedData: Buffer.from(ciphertext.toString()).toString('base64'),
+            keyId,
+        };
+    });
+
+} 
 
 export default async function handler(req, res) {
 
-    const {metadata,encryptedData,amount,source,idempotencyKey,keyId} = req.body;
-
+    // Get PCI Public Key 
     await sdk.auth(process.env.apikey);
+    const publickey = await sdk.getPublicKey(); 
+
+
+    // Encrypt Details 
+       const decodedPublicKey = await readKey({
+        armoredKey: Buffer.from(publicKey, 'base64').toString(),
+    });
+
+    const message = await createMessage({
+        text: JSON.stringify(dataToEncrypt),
+    });
+
+    //const encryptedDatas = await encrypt({message,encryptKeys: decodedPublicKey})
+
+    const encryptCard = await encryptDetails();
+
+    const {billingDetails,metadata,encryptedData,expMonth,expYear,keyId} = req.body;
+    const cardDetails =  await sdk.createCard({
+        billingDetails: billingDetails,
+        metadata: metadata,
+        idempotencyKey: 'ba943ff1-ca16-49b2-ba55-1057e70ca5c7',
+        keyId: keyId,
+        encryptedData: encryptedData, 
+        expMonth: expMonth,
+        expYear: expYear
+    })
+
+
+
+    // return encrypt({
+    //     message,
+    //     encryptionKeys: decodedPublicKey,
+    // }).then((ciphertext) => {
+    //     return {
+    //         encryptedData: Buffer.from(ciphertext.toString()).toString('base64'),
+    //         keyId,
+    //     };
+    // });
+
+    // Start Payment
+
+
+  
+   // const {metadata,encryptedData,amount,source,idempotencyKey,keyId} = req.body;
 
     const paymentResponse = await sdk.creatPayment({
         metadata: metadata,
