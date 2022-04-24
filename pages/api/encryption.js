751d1231d1ac8fa800e199c1a01b2835380ce1a4
Shivam <chotu.kv@gmail.com>
2022-04-24 10:26:43 +0530

    TS Fix

---
+++ pages/api/encryption.js
@@ -0,0 +1,23 @@
+/**
+ * Encrypt card data function
+ */
+export async function encryptCard(
+    dataToEncrypt: CardDetails): Promise<EncryptedValue> {
+    const decodedPublicKey = await readKey({
+        armoredKey: Buffer.from(publicKey, 'base64').toString(),
+    });
+
+    const message = await createMessage({
+        text: JSON.stringify(dataToEncrypt),
+    });
+
+    return encrypt({
+        message,
+        encryptionKeys: decodedPublicKey,
+    }).then((ciphertext) => {
+        return {
+            encryptedData: Buffer.from(ciphertext.toString()).toString('base64'),
+            keyId,
+        };
+    });
+}
