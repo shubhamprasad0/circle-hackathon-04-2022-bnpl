68293d642eaa73b0bbf8e294c08d72342d2cc6cf
shivam <chotu.kv@gmail.com>
2022-04-30 15:29:25 +0530

    WIP (#8)
    * WIP
    
    * Final Payment

--- pages/api/payment.js
+++ pages/api/payment.js
@@ -43,22 +43,46 @@ export default async function handler(req, res) {
 
 
     // Get Encryption Details
-    const encryptCard = await encryptDetails(publickey['data']['keyId'],publickey['data']['publicKey'],{cardNumber,CVV});
-
-
-    // Create Card Payment 
-
-    sdk2.auth(process.env.apikey);
-    const {billingDetails,metadata,expMonth,expYear,keyId,amount} = req.body;
+    //const encryptCard ;
+    try{
+   // const encryptCard = await encryptDetails(publickey['data']['keyId'],publickey['data']['publicKey'],{cardNumber,CVV});
+   const encryptCard = 'LS0tLS1CRUdJTiBQR1AgTUVTU0FHRS0tLS0tCgp3Y0JNQTBYV1NGbEZScFZoQVFmL2J2bVVkNG5LZ3dkbExKVTlEdEFEK0p5c0VOTUxuOUlRUWVGWnZJUWEKMGgzQklpRFNRU0RMZmI0NEs2SXZMeTZRbm54bmFLcWx0MjNUSmtPd2hGWFIrdnNSMU5IbnVHN0lUNWJECmZzeVdleXlNK1JLNUVHV0thZ3NmQ2tWamh2NGloY29xUnlTTGtJbWVmRzVaR0tMRkJTTTBsTFNPWFRURQpiMy91eU1zMVJNb3ZiclNvbXkxa3BybzUveWxabWVtV2ZsU1pWQlhNcTc1dGc1YjVSRVIraXM5ckc0cS8KMXl0M0FOYXA3UDhKekFhZVlyTnVNZGhGZFhvK0NFMC9CQnN3L0NIZXdhTDk4SmRVUEV0NjA5WFRHTG9kCjZtamY0YUtMQ01xd0RFMkNVb3dPdE8vMzVIMitnVDZKS3FoMmtjQUQyaXFlb3luNWcralRHaFNyd3NKWgpIdEphQWVZZXpGQUVOaFo3Q01IOGNsdnhZVWNORnJuNXlMRXVGTkwwZkczZy95S3loclhxQ0o3UFo5b3UKMFVxQjkzQURKWDlJZjRBeVQ2bU9MZm9wUytpT2lLall4bG1NLzhlVWc3OGp1OVJ5T1BXelhyTzdLWTNHClFSWm8KPXc1dEYKLS0tLS1FTkQgUEdQIE1FU1NBR0UtLS0tLQo' 
+   sdk2.auth(process.env.apikey);
+    const {billingDetails,metadata,expMonth,expYear,amount} = req.body;
     const cardDetails =  await sdk2.createCard({
         billingDetails: billingDetails,
         metadata: metadata,
         idempotencyKey: idempotencKey,
-        keyId: keyId,
+        keyId: publickey['data']['keyId'],
         encryptedData: encryptCard,
         expMonth: expMonth,
         expYear: expYear
     })
+console.log(cardDetails)
+   const source = {
+        id: cardDetails['data']['id'],
+        type: 'card'
+   }
+     const paymentResponse = await sdk2.createPayment({
+      metadata: metadata,
+      amount: amount,
+      autoCapture: true,
+      source: source,
+      idempotencyKey: idempotencKey,
+      keyId: publickey['data']['keyId'],
+      verification: 'none',
+      description: 'Payment',
+      encryptedData: encryptCard
+  //channel: 'ba943ff1-ca16-49b2-ba55-1057e70ca5c7'
+})
+      res.status(200).json({ paymentResponse });
+  }catch(e){
+  res.status(500).json({ e })
+  ;    }
+
+    // Create Card Payment 
+
+
        
 
     
@@ -99,10 +123,10 @@ export default async function handler(req, res) {
   
    // const {metadata,encryptedData,amount,source,idempotencyKey,keyId} = req.body;
 
-   const source = {
-        id: cardDetails['data']['id'],
-        type: 'card'
-   }
+  //  const source = {
+  //       id: cardDetails['data']['id'],
+  //       type: 'card'
+  //  }
 //    try{
 //     const paymentResponse = await sdk2.createPayment({
 //       metadata: {
@@ -125,23 +149,23 @@ export default async function handler(req, res) {
 //     res.status(500).json({ error });
 //   }
    
-   try{
-    const paymentResponse = await sdk2.createPayment({
-      metadata: metadata,
-  amount: amount,
-  autoCapture: true,
-  source: source,
-  idempotencyKey: idempotencKey,
-  keyId: 'key1',
-  verification: 'none',
-  description: 'Payment',
-  encryptedData: encryptCard
-  //channel: 'ba943ff1-ca16-49b2-ba55-1057e70ca5c7'
-})
-      res.status(200).json({ paymentResponse });
-  } catch(error){
-    res.status(500).json({ error });
-  }
+//    try{
+//     const paymentResponse = await sdk2.createPayment({
+//       metadata: metadata,
+//   amount: amount,
+//   autoCapture: true,
+//   source: source,
+//   idempotencyKey: idempotencKey,
+//   keyId: publickey['data']['keyId'],
+//   verification: 'none',
+//   description: 'Payment',
+//   encryptedData: encryptCard
+//   //channel: 'ba943ff1-ca16-49b2-ba55-1057e70ca5c7'
+// })
+//       res.status(200).json({ paymentResponse });
+//   } catch(error){
+//     res.status(500).json({ error });
+//   }
    
 
 
